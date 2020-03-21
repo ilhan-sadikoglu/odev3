50151160ca8eba95f88992e5bc7b6f8f41f74a81
ilhan-sadikoglu <56480436+ilhan-sadikoglu@users.noreply.github.com>
2020-03-21 19:54:33 +0300

    Kısıtlamalara uygun şekilde güncellendi.

---
+++ ödev3.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <chrono>
+
+using namespace std;
+
+//aþaðýdaki satýrlarý yoruma alarak kodunuzu farklý þekillerde test edebilirsiniz.
+#define _TEST_ITERATIVE_
+#define _TEST_RECURSIVE_
+
+// Bu odevde global veya statik degisken tanimlanamaz!!!
+
+/* Ozyinelemeli olarak zip zip kurbaganin kac farkli yoldan gittigini bulan fonksiyondur.
+ * Bu fonksiyon icinde for ve while kullanamazsiniz ve en az bir kez recursiveFunction
+ * fonksiyonu cagri yapilmalidir.
+ */
+long long recursiveFunction(int n)
+{
+	
+	//2 , 0 ve 1 özel durum-basecase olur 
+	if (n == 2)
+		return 2;
+
+	if (n == 0)
+		return 1;
+
+	if (n == 1)
+		return 1;
+
+	//diðer durumlarda
+		return (recursiveFunction(n - 3) + recursiveFunction(n - 2)) + recursiveFunction(n - 1);
+
+}
+
+/* Yinelemeli olarak zip zip kurbaganin kac farkli yoldan gittigini bulan fonksiyondur.
+ * Bu fonksiyon icinde for ve while dongusu kullanilmalidir.
+ * Bu fonksiyon icinde baska bir fonksiyona (kendisi dahil) cagri yapilamaz.
+ */
+long long iterativeFunction(int n)
+{
+	int d[100]{ 0 };
+
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
+
+	for (int i=4;i<=n;i++) {
+
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+
+	}
+
+	return d[n];
+}
+
+int main()
+{
+	const int MAX_NUMBER_OF_STONES{ 72 };
+
+	cout << setfill(' ');
+
+	cout.imbue(locale(""));
+
+	for (auto i = 1; i <= MAX_NUMBER_OF_STONES; ++i)
+	{
+
+#ifdef _TEST_ITERATIVE_
+		{
+			long long tempResult = { 0 };
+
+			auto start = std::chrono::steady_clock::now();
+			tempResult = iterativeFunction(i);
+			auto stop = std::chrono::steady_clock::now();
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+
+
+			cout << "iterative" << setw(4) << i << " -> " << tempResult << " ( " << duration << "us )\n";
+		}
+#endif
+
+#ifdef _TEST_RECURSIVE_
+		{
+			long long tempResult = { 0 };
+
+			auto start = std::chrono::steady_clock::now();
+			tempResult = recursiveFunction(i);
+			auto stop = std::chrono::steady_clock::now();
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+
+			cout << "recursive" << setw(4) << i << " -> " << tempResult << " ( " << duration << "us )\n\n";
+		}
+#endif
+
+	}
+
+	return 0;
+}
